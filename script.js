feb028bc5daa0c432a0b3e10d7e0a04e171866a0
Sebastian Baldini <sebasbaldini@gmail.com>
2023-02-22 13:15:32 -0300

    8

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(() =>{
 // funcion de mapa
 var map = L.map('map').setView([-34.61315, -58.37723], 10);
 var marker = L.marker([-34.61315, -58.37723]).addTo(map);
-var marker2 = L.marker([-34.61315, -51]).addTo(map);
+
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
