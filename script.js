990f5e2fe178feb5fee0aeea76fb0af63579d67c
Sebastian Baldini <sebasbaldini@gmail.com>
2023-02-22 13:40:27 -0300

    9

--- script.js
+++ script.js
@@ -18,20 +18,19 @@ $(document).ready(() =>{
         $('#graficos').hide();
         $('#contactos').show();
                });
-    
 });
 
 // funcion de mapa
 var map = L.map('map').setView([-34.61315, -58.37723], 10);
 var marker = L.marker([-34.61315, -58.37723]).addTo(map);
-
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+// funcion api de bitcoin para probar
 const url = 'https://blockchain.info/ticker';
 $.ajax({
   url: url,
